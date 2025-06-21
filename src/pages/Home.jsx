3c43b7b7a549b92ad159a604d65945e8af3ed60e
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2025-06-21 23:27:17 +0530

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,26 @@
-import React from 'react'
-import HomeBanner from '../components/HomeBanner'
-import HeroSection from '../components/HeroSection'
-import CourseTabs from '../components/CourseTabs'
-import FeaturesSection from '../components/FeaturesSection'
-import FAQSection from '../components/FaqSection'
-import ImpactSection from '../components/ImpactSection'
-import TestimonialSlider from '../components/TestimonialSlider'
-import BlogSection from '../components/BlogSection'
+import HomeBanner from "../components/HomeBanner";
+import CourseTabs from "../components/CourseTabs";
+import FAQSection from "../components/FaqSection";
+import ImpactSection from "../components/ImpactSection";
+import TestimonialSlider from "../components/TestimonialSlider";
+import BlogSection from "../components/BlogSection";
 
-import MiniCard from '../components/MiniCard'
-import ContactFormSection from '../components/ContactFormSection'
+import MiniCard from "../components/MiniCard";
+import ContactFormSection from "../components/ContactFormSection";
 
 const Home = () => {
   return (
     <>
-    <HomeBanner/>
-    <HeroSection/>
-    <CourseTabs/> 
-    <MiniCard/>
-    <FAQSection/> 
-    <ImpactSection/>
-    <TestimonialSlider/>
-    <BlogSection/>
-    <ContactFormSection/>
+      <HomeBanner />
+      <CourseTabs />
+      <MiniCard />
+      <FAQSection />
+      <ImpactSection />
+      <TestimonialSlider />
+      <BlogSection />
+      <ContactFormSection />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
